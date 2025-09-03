174acc22a848e377d7afbcf8c224740dd7771c8d
Ciyasan <85624929+Ciyasan@users.noreply.github.com>
2025-09-03 09:34:01 +0200

    fix: routes (#17)
    Co-authored-by: Ciyasan <ciyasama@gmail.com>

--- app/Config/Routes/Admin.php
+++ app/Config/Routes/Admin.php
@@ -24,12 +24,12 @@ $routes->group('admin', ['namespace' => 'App\Controllers\Admin', 'filter' => 'au
     $routes->get('/', 'Recipe::index');
     $routes->get('(:num)', 'Recipe::edit/$1');
     $routes->get('new', 'Recipe::create');
+  });
 
   $routes->group('brand', function ($routes) {
     $routes->get('/', 'Brand::index');
     $routes->post('update', 'Brand::update');
     $routes->post('insert', 'Brand::insert');
     $routes->post('delete', 'Brand::delete');
-
   });
 });
